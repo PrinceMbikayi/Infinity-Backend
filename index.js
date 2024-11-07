732e80851d1e109c0aebe4064e53347ee93c1963
MuhammadHuzaifaSheikh <huzaifa900shaikh@gmail.com>
2024-11-08 01:55:37 +0500

    updated

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.use("/api/banner", bannerRouter);
 app.use('/api/payments', paymentRoute);
 // Route pour la racine
 app.get('/', (req, res) => {
-  res.send(`API is running on port...${PORT}` );
+  res.send(`API is running on port` );
 });
 
 // Gestion des erreurs
